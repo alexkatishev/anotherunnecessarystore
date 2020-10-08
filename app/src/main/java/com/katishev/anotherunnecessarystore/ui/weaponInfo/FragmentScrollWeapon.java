8fe727ccfc0e44fa8f781b34b451ad812e64cffd
Alex Catyshev <38359929+alexkatishev@users.noreply.github.com>
2020-10-08 19:50:51 +0300

    try Assets

--- app/src/main/java/com/katishev/anotherunnecessarystore/ui/weaponInfo/FragmentScrollWeapon.java
+++ app/src/main/java/com/katishev/anotherunnecessarystore/ui/weaponInfo/FragmentScrollWeapon.java
@@ -5,13 +5,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.katishev.anotherunnecessarystore.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 
 class FragmentScrollWeapon extends Fragment {
+    InputStream inputStream;
+    TextView infoScrollWeapon;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        infoScrollWeapon = getView().findViewById(R.id.tv_scroll_info_weapon);
+        try(Scanner scanner =new Scanner(inputStream).useDelimiter("")){
+          infoScrollWeapon.setText( scanner.hasNext() ? scanner.next() : ""); }
+        try {
+         inputStream = getActivity().getAssets().open("info_weapon.xml");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Nullable
     @Override
@@ -20,8 +44,4 @@ class FragmentScrollWeapon extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_scroll_weapon, container, false);
     }
-
-
-
-
 }
